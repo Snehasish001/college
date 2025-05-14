5b579e704d2195a1c640dbed774c58daa1d3881b
Snehasish001 <professionalsnehasish001@gmail.com>
2025-05-14 22:18:30 +0530

    Templates Added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('api/', include('core_api.urls')),
 ]
 
